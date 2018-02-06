143e183894a2d5b650e2d68d15245d0a581451a4
Andy Chin <andy.chin@gamblitgaming.com>
2018-02-05 17:28:21 -0800

    Forgot to add Project.cs file

---
+++ Project.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JIRA_Bug_List_Creator
+{
+    public class Project
+    {
+        private string strProjectName;
+        private List<JIRAIssue> JIRAIssues;
+
+        public string ProjectName
+        {
+            get
+            {
+                return this.strProjectName;
+            }
+            set
+            {
+                this.strProjectName = value;
+            }
+        }
+
+        public List<JIRAIssue> Issues
+        {
+            get
+            {
+                return this.JIRAIssues;
+            }
+            set
+            {
+                this.JIRAIssues = value;
+            }
+        }
+
+        public Project(string project)
+        {
+            this.strProjectName = project;
+            this.JIRAIssues = new List<JIRAIssue>();
+        }
+    }
+}
